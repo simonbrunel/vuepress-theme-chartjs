72907dba2b6aba30e989e087e7c4ed1495163ca9
Simon Brunel <simonbrunel@users.noreply.github.com>
2021-03-29 22:11:37 +0200

    Fix code block highlight issues caused by Prism

--- enhanceApp.js
+++ enhanceApp.js
@@ -1,4 +1,16 @@
-// This is required by vue-prism-editor/dist/prismeditor.esm.js
-if (typeof window !== 'undefined' && window.global === undefined) {
-  window.global = window;
+// Note that prismjs comes as a 'vuepress' dependency.
+// https://github.com/vuejs/vuepress/blob/master/packages/%40vuepress/markdown/package.json#L31
+
+if (typeof window !== 'undefined') {
+  // BUG(brunel): Importing prismjs from the EditorTextarea component causes issues when
+  // mixing chart editors with regular code blocks. It's due to the attempt to highlight
+  // all code blocks after the document has loaded. Setting manual=true fixes the issue.
+  // https://prismjs.com/docs/Prism.html#.manual
+  window.Prism = window.Prism || {};
+  window.Prism.manual = true;
+
+  // This is required by vue-prism-editor/dist/prismeditor.esm.js
+  if (window.global === undefined) {
+    window.global = window;
+  }
 }
